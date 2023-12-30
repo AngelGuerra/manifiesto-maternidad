1e50859c7df323ef778ae21ebd343ebd85da02bb
Ángel Guerra <a.guerrarevilla@gmail.com>
2023-12-30 15:28:12 +0100

    Generates new version of the page
    - Removes propuestas and manifiesto urls.
    - Integrates propuestas and manifiesto into the home page.
    - Adds Netlify redirects.
    - Adds import aliases.
    - Upgrades Astro.
    - Upgrades Node version.
    - Removes unnecesary files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.manifiestoporlasmadres.org",
-	trailingSlash: "always",
-	integrations: [
-		tailwind(),
-		alpinejs(),
-		sitemap({
-			filter: (page) =>
-				page !==
-					"https://www.manifiestoporlasmadres.org/politica-de-cookies/" &&
-				page !==
-					"https://www.manifiestoporlasmadres.org/politica-de-privacidad-y-aviso-legal/",
-		}),
-	],
+  site: "https://www.manifiestoporlasmadres.org",
+  trailingSlash: "always",
+  integrations: [
+    alpinejs(),
+    sitemap({
+      filter: (page) =>
+        page !==
+          "https://www.manifiestoporlasmadres.org/politica-de-cookies/" &&
+        page !==
+          "https://www.manifiestoporlasmadres.org/politica-de-privacidad-y-aviso-legal/",
+    }),
+    tailwind(),
+  ],
 });
